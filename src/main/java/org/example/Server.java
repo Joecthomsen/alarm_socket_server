f247d2d12d09e7dafcf1ed4d2df31531b42a290a
joecthomsen <joecthomsen@gmail.com>
2023-04-28 15:36:57 +0200

    Simple socket - need to implement a protocol

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() throws IOException {
+        try{
+            while (!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("A client has connected!");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() throws IOException {
+        if(serverSocket != null){
+            serverSocket.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(1234);
+        Server server = new Server(socket);
+        server.startServer();
+    }
+
+/*    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(1234);
+        Server server = new Server(socket);
+    }*/
+}
